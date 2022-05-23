3c35ceed4489734b7eb3c5f512fad769835ecdbe
Schris <tovechris48@gmail.com>
2022-05-23 15:39:05 -0400

    install-inquirer

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const generatePage = require('./src/page-template.js');
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
+// const generatePage = require('./src/page-template.js');
 
-const [firstName, github] = profileDataArgs;
+// const pageHTML = generatePage(title);
 
-fs.writeFile('./index.html', generatePage(firstName, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
